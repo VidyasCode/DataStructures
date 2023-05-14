a34390c41cfac6c281cc00ea52d74bf7118f97a1
ngvidya <82347212+ngvidya@users.noreply.github.com>
2023-05-14 18:34:54 -0400

    Adding remove function

--- double-linked-list.js
+++ double-linked-list.js
@@ -18,14 +18,47 @@ class LinkedList {
             this.head = spot;
             this.tail = spot;
         } else {
+            //* move the connectors 
             this.tail.next = spot;
             spot.prev = this.tail;
 
+            //* at the end move the tail pointer
             this.tail = spot;
 
         }
     }
 
+    remove(value){
+        for(let current= this.head; current; current= current.next){
+            if(current.value === value){
+
+                //* if it is the only node
+                if(this.head === this.tail){
+                    this.head =null;
+                    this.tail=null;
+                    return;
+                }
+
+                //* If removing node is at head
+                if(current === this.head){
+                    this.head = this.head.next;
+                    this.head.prev = null;
+                    return;
+                }
+                //* If node is at the tail
+                if(current === this.tail ){
+                    this.tail = this.tail.prev;
+                    
+                    return;
+                }
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+                return true;
+            }
+            
+        }
+        return false;
+    }
     printForward() {
         console.log('Print forward');
         for (let current = this.head; current !== null; current = current.next) {
@@ -44,6 +77,7 @@ class LinkedList {
 const list = new LinkedList();
 list.add(5);
 list.add(7);
-list.add(8);
+list.add(9);
 list.printForward();
+list.remove(9);
 list.printBackward();
