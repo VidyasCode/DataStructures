af6d94e417bda54b29d98aba6b1da84fef1ffb1a
ngvidya <82347212+ngvidya@users.noreply.github.com>
2023-05-14 20:30:27 -0400

    add and remove multiples

--- sentinel-list.js
+++ sentinel-list.js
@@ -1,16 +1,16 @@
 //* A list with a empty head and tail
 //* provides a way to add and remove nodes without checking for nulll head/tail
 
-class Node{
-    constructor(value){
+class Node {
+    constructor(value) {
         this.value = value;
         this.next = null;
         this.prev = null;
     }
 }
 
-class Sentinal{
-    constructor(){
+class Sentinal {
+    constructor() {
         this.head = new Node(undefined);
         this.tail = new Node(undefined);
 
@@ -18,30 +18,52 @@ class Sentinal{
         this.tail.prev = this.head;
     }
 
-    addToBack(value){
-        const spot = new Node(value);
+    addToBack(...values) {
+        values.forEach(value => {
+            const spot = new Node(value);
 
-        spot.next = this.tail;
-        spot.prev = this.tail.prev;
+            spot.next = this.tail;
+            spot.prev = this.tail.prev;
 
-        this.tail.prev.next = spot;
-        this.tail.prev = spot;
-      
-    }
-
-    addToFront(value){
-        const spot = new Node(value);
-
-        spot.next = this.head.next;
-        spot.prev = this.head;
-
-        this.head.next.prev = spot;
-        this.head.next = spot;
+            this.tail.prev.next = spot;
+            this.tail.prev = spot;
+        })
 
     }
 
-    printForward(){
-        for(let current = this.head.next; current !== this.tail; current= current.next){
+    addToFront(...values) {
+        values.forEach(value => {
+            const spot = new Node(value);
+
+            spot.next = this.head.next;
+            spot.prev = this.head;
+
+            this.head.next.prev = spot;
+            this.head.next = spot;
+        })
+
+    }
+
+    remove(...values) {
+        let count = [];
+        values.forEach(value => {
+            for (let current = this.head.next; current !== this.tail; current = current.next) {
+                if (current.value === value) {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                    
+                    count[value] ? count[value]++ : count[value]=1;
+                }
+            }
+        });
+        count.map((count, i) => {
+            console.log(`removed ${i}, ${count} occurences`);
+        });
+    }
+
+    printForward() {
+        console.log("printing forward");
+        for (let current = this.head.next; current !== this.tail; current = current.next) {
             console.log(current.value);
         }
     }
@@ -49,10 +71,9 @@ class Sentinal{
 }
 
 const list = new Sentinal();
-list.addToBack(5);
-list.addToBack(7);
-list.addToBack(9);
-list.addToFront(3);
-list.addToFront(4);
-list.addToBack(6);
+list.addToBack(5, 7, 4, 9, 1);
+list.addToFront(3, 4, 8,7);
+list.addToBack(7,6);
+list.printForward();
+list.remove(4,7);
 list.printForward();
